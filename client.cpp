b218c4842eaa3259479ef6c027bc5512a968f1c1
youssefezz12 <youssefezz@gmail.com>
2026-03-31 17:02:54 +0200

    Final build

---
+++ client.cpp
@@ -0,0 +1,30 @@
+#include <array>
+#include <boost/asio.hpp>
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+int main() {
+    boost::asio::io_context io_context;
+
+    tcp::endpoint endpoint(boost::asio::ip::make_address("127.0.0.1"), 13);
+
+    tcp::socket socket(io_context);
+    socket.connect(endpoint);
+
+    while (true) {
+        std::array<char, 128> buf;
+        boost::system::error_code error;
+
+        size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+        if (error == boost::asio::error::eof)
+            break;
+        else if (error)
+            throw boost::system::system_error(error);
+
+        std::cout.write(buf.data(), len);
+    }
+
+    return 0;
+}
